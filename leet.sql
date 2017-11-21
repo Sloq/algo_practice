1852508ab539529c25702d20bc2259539b17e404
Stephen Loquet <sloquet@gmail.com>
2017-11-20 19:31:13 -0800

    complete count sql query

---
+++ leet.sql
@@ -0,0 +1,45 @@
+-- The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+
+-- +----+-------+--------+-----------+
+-- | Id | Name  | Salary | ManagerId |
+-- +----+-------+--------+-----------+
+-- | 1  | Joe   | 70000  | 3         |
+-- | 2  | Henry | 80000  | 4         |
+-- | 3  | Sam   | 60000  | NULL      |
+-- | 4  | Max   | 90000  | NULL      |
+-- +----+-------+--------+-----------+
+-- Given the Employee table, write a SQL query that finds out employees who earn more than their managers. For the above table, Joe is the only employee who earns more than his manager.
+
+-- +----------+
+-- | Employee |
+-- +----------+
+-- | Joe      |
+-- +----------+
+
+SELECT A.Name as Employee
+FROM Employee A, Employee B
+WHERE A.ManagerId = B.Id
+AND A.Salary > B.Salary
+
+-- Write a SQL query to find all duplicate emails in a table named Person.
+
+-- +----+---------+
+-- | Id | Email   |
+-- +----+---------+
+-- | 1  | a@b.com |
+-- | 2  | c@d.com |
+-- | 3  | a@b.com |
+-- +----+---------+
+-- For example, your query should return the following for the above table:
+
+-- +---------+
+-- | Email   |
+-- +---------+
+-- | a@b.com |
+-- +---------+
+
+SELECT Email 
+FROM Person 
+-- WHERE COUNT(Email) > 1
+GROUP BY Email
+HAVING COUNT(Email) > 1
