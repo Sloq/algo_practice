a4a1228d3b10fadd6d06b6bd2a15c9138bb9f350
Stephen Loquet <sloquet@gmail.com>
2017-11-21 19:08:47 -0800

    complete customers who dont order problem

--- leet.sql
+++ leet.sql
@@ -40,6 +40,39 @@ AND A.Salary > B.Salary
 
 SELECT Email 
 FROM Person 
--- WHERE COUNT(Email) > 1
 GROUP BY Email
 HAVING COUNT(Email) > 1
+
+-- Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+
+-- Table: Customers.
+
+-- +----+-------+
+-- | Id | Name  |
+-- +----+-------+
+-- | 1  | Joe   |
+-- | 2  | Henry |
+-- | 3  | Sam   |
+-- | 4  | Max   |
+-- +----+-------+
+-- Table: Orders.
+
+-- +----+------------+
+-- | Id | CustomerId |
+-- +----+------------+
+-- | 1  | 3          |
+-- | 2  | 1          |
+-- +----+------------+
+-- Using the above tables as example, return the following:
+
+-- +-----------+
+-- | Customers |
+-- +-----------+
+-- | Henry     |
+-- | Max       |
+-- +-----------+
+
+SELECT Customers.Name AS Customers
+FROM Customers
+LEFT JOIN Orders ON Customers.Id = Orders.CustomerId
+WHERE Orders.CustomerId IS Null
