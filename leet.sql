6c3afc17ad2c0fd79b0292e861710c3b1ef21d0a
Stephen Loquet <sloquet@gmail.com>
2017-11-28 23:39:16 -0800

    complete highest department employee sql query

--- leet.sql
+++ leet.sql
@@ -76,3 +76,44 @@ SELECT Customers.Name AS Customers
 FROM Customers
 LEFT JOIN Orders ON Customers.Id = Orders.CustomerId
 WHERE Orders.CustomerId IS Null
+
+-- The Employee table holds all employees. Every employee has an Id, a salary, and there is also a column for the department Id.
+
+-- +----+-------+--------+--------------+
+-- | Id | Name  | Salary | DepartmentId |
+-- +----+-------+--------+--------------+
+-- | 1  | Joe   | 70000  | 1            |
+-- | 2  | Henry | 80000  | 2            |
+-- | 3  | Sam   | 60000  | 2            |
+-- | 4  | Max   | 90000  | 1            |
+-- +----+-------+--------+--------------+
+-- The Department table holds all departments of the company.
+
+-- +----+----------+
+-- | Id | Name     |
+-- +----+----------+
+-- | 1  | IT       |
+-- | 2  | Sales    |
+-- +----+----------+
+-- Write a SQL query to find employees who have the highest salary in each of the departments. For the above tables, Max has the highest salary in the IT department and Henry has the highest salary in the Sales department.
+
+-- +------------+----------+--------+
+-- | Department | Employee | Salary |
+-- +------------+----------+--------+
+-- | IT         | Max      | 90000  |
+-- | Sales      | Henry    | 80000  |
+-- +------------+----------+--------+
+
+-- SELECT Department.Name as Department, Employee.Name as Employee, Employee.Salary 
+-- FROM Employee 
+-- JOIN Department ON Employee.DepartmentId = Department.Id
+
+
+SELECT D.Name AS Department ,E.Name AS Employee ,E.Salary 
+FROM
+	Employee E,
+	(SELECT DepartmentId,max(Salary) as max FROM Employee GROUP BY DepartmentId) T,
+	Department D
+WHERE E.DepartmentId = T.DepartmentId 
+  AND E.Salary = T.max
+  AND E.DepartmentId = D.id
