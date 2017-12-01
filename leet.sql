a65cdc8f5c8edc84a987bb85835d08baa841f599
Stephen Loquet <sloquet@gmail.com>
2017-11-30 23:45:53 -0800

    complete weather sql

--- leet.sql
+++ leet.sql
@@ -117,3 +117,27 @@ FROM
 WHERE E.DepartmentId = T.DepartmentId 
   AND E.Salary = T.max
   AND E.DepartmentId = D.id
+
+
+-- Given a Weather table, write a SQL query to find all dates' Ids with higher temperature compared to its previous (yesterday's) dates.
+
+-- +---------+------------+------------------+
+-- | Id(INT) | Date(DATE) | Temperature(INT) |
+-- +---------+------------+------------------+
+-- |       1 | 2015-01-01 |               10 |
+-- |       2 | 2015-01-02 |               25 |
+-- |       3 | 2015-01-03 |               20 |
+-- |       4 | 2015-01-04 |               30 |
+-- +---------+------------+------------------+
+-- For example, return the following Ids for the above Weather table:
+-- +----+
+-- | Id |
+-- +----+
+-- |  2 |
+-- |  4 |
+-- +----+
+
+SELECT w1.Id 
+FROM Weather w1, Weather w2
+WHERE w1.Temperature > w2.Temperature AND 
+      TO_DAYS(w1.DATE)-TO_DAYS(w2.DATE)=1;
