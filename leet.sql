4f615fd1832f05addb37a53c58a2ea82e3159ab2
Stephen Loquet <sloquet@gmail.com>
2017-12-03 21:13:21 -0800

    complete sql query

--- leet.sql
+++ leet.sql
@@ -141,3 +141,30 @@ SELECT w1.Id
 FROM Weather w1, Weather w2
 WHERE w1.Temperature > w2.Temperature AND 
       TO_DAYS(w1.DATE)-TO_DAYS(w2.DATE)=1;
+
+-- Write a SQL query to get the nth highest salary from the Employee table.
+
+-- +----+--------+
+-- | Id | Salary |
+-- +----+--------+
+-- | 1  | 100    |
+-- | 2  | 200    |
+-- | 3  | 300    |
+-- +----+--------+
+-- For example, given the above Employee table, the nth highest salary where n = 2 is 200. If there is no nth highest salary, then the query should return null.
+
+-- +------------------------+
+-- | getNthHighestSalary(2) |
+-- +------------------------+
+-- | 200                    |
+-- +------------------------+
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+DECLARE M INT;
+SET M=N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT M, 1
+  );
+END
